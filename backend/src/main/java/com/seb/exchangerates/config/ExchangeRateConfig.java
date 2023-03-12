741b8399feb14a4e9f9c65a8780ca55fb543bc27
Dimitris Bouzoukas <dimbouzoukas@gmail.com>
2023-03-13 00:31:02 +0200

    Progress

--- backend/src/main/java/com/seb/exchangerates/config/ExchangeRateConfig.java
+++ backend/src/main/java/com/seb/exchangerates/config/ExchangeRateConfig.java
@@ -13,14 +13,14 @@ public class ExchangeRateConfig {
     Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
     // this package must match the package in the <generatePackage> specified in
     // pom.xml
-    marshaller.setContextPath("http://www.lb.lt/WebServices/ExchangeRates/ExchangeRates.asmx?WSDL");
+    marshaller.setContextPath("com.seb.exchangerates.model");
     return marshaller;
   }
 
   @Bean
   public ExchangeRateService setupExchangeRateClient(Jaxb2Marshaller marshaller) {
     ExchangeRateService client = new ExchangeRateService();
-    client.setDefaultUri("http://localhost:8080/ws");
+    client.setDefaultUri("http://localhost:8080/wsdl");
     client.setMarshaller(marshaller);
     client.setUnmarshaller(marshaller);
     return client;
--- backend/src/main/java/com/seb/exchangerates/model/ExchangeRate.java
+++
@@ -1,4 +0,0 @@
-package com.seb.exchangerates.model;
-
-/** ExchangeRate */
-public class ExchangeRate {}
