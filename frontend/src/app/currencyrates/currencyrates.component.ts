367813f4150ccf07f85798581d76ef794e693332
Dimitris Bouzoukas <dimbouzoukas@gmail.com>
2023-03-15 00:37:19 +0200

    Polishing front end

--- frontend/src/app/currencyrates/currencyrates.component.ts
+++ frontend/src/app/currencyrates/currencyrates.component.ts
@@ -14,10 +14,14 @@ import { ExchangeRateDifference } from './exchangerate-difference';
 export class CurrencyTableComponent {
     currencyrates: ExchangeRateDifference[] | null = null;
     date: string = '';
+    dateError: boolean | false = false;
 
     constructor(private http: HttpClient) { }
 
     onSubmit() {
+
+        if (this.date == '' || this.dateError == true) return;
+
         const url = `http://localhost:8080?date=${this.date}`;
         this.http.get<any[]>(url).pipe(
             map(data => data.map(rate => ({
@@ -29,5 +33,12 @@ export class CurrencyTableComponent {
         ).subscribe((data: ExchangeRateDifference[]) => {
             this.currencyrates = data;
         });
+    }
+
+    validateDate() {
+        const minDate = new Date("1993-06-29");
+        const maxDate = new Date("2014-12-31");
+        const inputDate = new Date(this.date);
+        this.dateError = inputDate < minDate || inputDate > maxDate;
     }
 }
